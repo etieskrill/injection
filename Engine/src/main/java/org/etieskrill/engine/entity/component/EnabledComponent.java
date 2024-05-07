87a04a5143034f1cec992246f400eb653da54b7f
Etienne <etiebr@gmail.com>
2024-05-07 16:58:36 +0200

    minor changes

--- Engine/src/main/java/org/etieskrill/engine/entity/component/EnabledComponent.java
+++
@@ -1,7 +0,0 @@
-package org.etieskrill.engine.entity.component;
-
-public class EnabledComponent extends Component<Boolean> {
-    public EnabledComponent(int id, Boolean component) {
-        super(id, component);
-    }
-}
