1c515ac3ee29788b4d8e2fa6cd8a529b8b2b6695
Etienne <etiebr@gmail.com>
2025-01-08 22:06:43 +0100

    feat: add generated sources to source set
    Signed-off-by: Etienne <etiebr@gmail.com>

--- shader-reflection/build.gradle.kts
+++ shader-reflection/build.gradle.kts
@@ -1,2 +1,7 @@
 group = "org.etieskrill.engine.extension"
 version = "1.0.0-SNAPSHOT"
+
+dependencies {
+    implementation("org.joml:joml:1.10.8")
+    implementation("org.joml:joml-primitives:1.10.0")
+}
