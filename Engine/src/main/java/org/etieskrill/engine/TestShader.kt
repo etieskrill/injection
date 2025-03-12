0c3f4be5f901dd939e0521124584a3e94a498c39
Etienne <etiebr@gmail.com>
2025-03-12 19:26:28 +0100

    shader dsl: trying for a compiler plugin
    Signed-off-by: Etienne <etiebr@gmail.com>

--- Engine/src/main/java/org/etieskrill/engine/TestShader.kt
+++
@@ -1,7 +0,0 @@
-package org.etieskrill.engine
-
-import org.etieskrill.engine.graphics.gl.shader.ShaderProgram
-
-class TestShader : ShaderProgram(
-    listOf("Test.glsl")
-)
--- shader-kotlin-dsl/build.gradle.kts
+++ shader-kotlin-dsl/build.gradle.kts
@@ -1,13 +1,28 @@
 plugins {
     `java-library`
-    kotlin("jvm")
+    `java-gradle-plugin`
+    kotlin("jvm") version "2.1.0" //TODO find out why tf this does not work without a version and why the fucking logs do not tell you shit about this
+//    `kotlin-dsl`
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29"
 }
 
-group = "io.github.etieskrill.extension.shader.dsl"
-version = "unspecified"
+group = "io.github.etieskrill.injection.extension.shader.dsl"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
+    maven { url = uri("https://www.jetbrains.com/intellij-repository/releases") }
+    maven { url = uri("https://cache-redirector.jetbrains.com/intellij-dependencies") }
+}
+
+gradlePlugin {
+    plugins {
+        create("ShaderDslPlugin") {
+            id = "io.github.etieskrill.injection.shader.dsl"
+            implementationClass = "io.github.etieskrill.injection.extension.shader.dsl.ShaderDslPlugin"
+        }
+    }
 }
 
 dependencies {
@@ -19,8 +34,21 @@ dependencies {
 
     runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:2.1.10")
 
+//    compileOnly("com.google.auto.service:auto-service:1.1.1")
+//    ksp("com.google.auto.service:auto-service:1.1.1")
+
+    compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.1.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin-api:2.1.10")
+
     testImplementation(kotlin("test"))
 }
+
+//FIXME i don't think a dependency will do
+//tasks.withType<KotlinCompilationTask<*>> {
+//    compilerOptions.freeCompilerArgs.add(
+//        "-Xplugin=${project.buildDir}/libs/shader-kotlin-dsl-1.0.0-SNAPSHOT.jar"
+//    )
+//}
 
 tasks.test {
     useJUnitPlatform()
